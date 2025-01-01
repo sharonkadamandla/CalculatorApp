d343796b60d8d9e0c5387dc4c6a5e20fef50f043
Sharon K <your.email@example.com>
2025-01-01 14:32:31 -0500

    App created on Android Studio

---
+++ KotlinCalculator/app/src/main/java/com/apps/kotlincalculator/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.apps.kotlincalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+
+    lateinit var btnAdd : Button
+    lateinit var btnSub : Button
+    lateinit var btnMultiply : Button
+    lateinit var btnDivision : Button
+    lateinit var etA : EditText
+    lateinit var etB : EditText
+    lateinit var resultTV : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnAdd = findViewById(R.id.btn_add)
+        btnSub = findViewById(R.id.btn_subtract)
+        btnMultiply = findViewById(R.id.btn_multiply)
+        btnDivision = findViewById(R.id.btn_divide)
+        etA = findViewById(R.id.et_a)
+        etB = findViewById(R.id.et_b)
+        resultTV = findViewById(R.id.result_tv)
+
+        btnAdd.setOnClickListener(this)
+        btnSub.setOnClickListener(this)
+        btnMultiply.setOnClickListener(this)
+        btnDivision.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        var a  = etA.text.toString().toDouble()
+        var b  = etB.text.toString().toDouble()
+        var result = 0.0
+        var inputInString = " "
+
+        when(v?.id){
+            R.id.btn_add ->{
+                result = a + b;
+                inputInString = "$a + $b = "
+            }
+            R.id.btn_subtract ->{
+                result = a - b;
+                inputInString = "$a - $b = "
+            }
+            R.id.btn_multiply ->{
+                result = a * b;
+                inputInString = "$a * $b = "
+            }
+            R.id.btn_divide ->{
+                result = a / b;
+                inputInString = "$a / $b = "
+            }
+        }
+        resultTV.text = "$inputInString $result"
+    }
+}
+
